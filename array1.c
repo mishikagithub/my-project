3efb96d5e178577222434005b34015577b5a87f8
mishikagithub <96657297+mishikagithub@users.noreply.github.com>
2023-02-20 23:56:36 +0530

    Add files via upload

---
+++ array1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a[4];
+    printf("enter four number \n");
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%d", a[i]);
+        scanf("%d", &a[i]);
+    
+    }
+    getch();
+    
+}
